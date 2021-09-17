4222c80d12a0123e958acca3c822dfb81c4d09d9
kimyoungjin98 <gyu250@naver.com>
2021-09-17 16:20:24 +0900

    tic-tac-toe

--- react-tic-tac/src/comps/Board.jsx
+++ react-tic-tac/src/comps/Board.jsx
@@ -1,25 +1,50 @@
 import React, { useState } from "react";
-import RenderSquare from "../modules/Main";
+import {RenderSquare, calcWinner} from "../modules/Main";
 
 function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [oxFlag, setOxFlag] = useState(true);
 
   //   const change = (_squares) => {
   //     setSquares([..._squares, squares]);
   //   };
 
-  const change = (num, index) => {
-    const _squares = [...squares];
-    _squares[num] = index % 2 == 0 ? "X" : "O";
-    setSquares(_squares);
-  };
+  const change = (num, count) => {
 
+    if(calcWinner(squares)) return;
+
+    // if(문자열변수) : 문자열 변수 값이 null, undefinded, "" 이면 무조건 false
+    // 아니면 true
+    if(squares[num]) return;
+
+    // count++
+    const _squares = [...squares];
+    // _squares[num] = count % 2 == 0 ? "X" : "O";
+    _squares[num] = oxFlag ? "O" : "X"
+    setSquares(_squares);
+    setOxFlag(!oxFlag)    
+  };
+  const player = oxFlag ? "O" : "X"
+  const winner = calcWinner(squares)
+  const msg = winner ? `winner : ${winner}` : `player : ${player}` 
+
+  const onRestart = ()=>{
+    let active = document.querySelectorAll(".active")
+    for(let i = 0 ; i < active.length ; i++){
+      active[i].classList.remove("active"); 
+    }
+    
+    setSquares(Array(9).fill(""))
+    setOxFlag(!oxFlag)
+    
+    // setSquares(Array(9).fill(null))
+  }
+  
   return (
     <div className="btn_box">
-      <div>
-        <h3>다음 플레이어 : 0</h3>
-      </div>
-      <RenderSquare squares={squares} change={change} />
+      <RenderSquare squares={squares} change={change}/>
+      <h3>{msg}</h3>
+      <div>{winner ? (<button className="btn_restart" onClick={onRestart}>다시 시작</button>) : "" }</div>
       {/* <div>{RenderSquare(squares)}</div> */}
     </div>
   );
--- react-tic-tac/src/modules/Main.jsx
+++ react-tic-tac/src/modules/Main.jsx
@@ -31,7 +31,7 @@ const arrayEx = () => {
    */
 };
 
-let index = 0;
+let count = 0;
 const RenderSquare = (props) => {
   const { squares, change } = props;
 
@@ -42,13 +42,17 @@ const RenderSquare = (props) => {
   ];
 
   const onButtonClick = (e) => {
-    index++;
+    const num = e.target.dataset.num;
+    e.target.classList.add("active")
+    // if(squares[num] != null && squares[num] === ""){
+    //   return;
+    // }
+    // count++
     // const tg = e.target;
     // if (tg.tagName === "BUTTON") {
-    const num = e.target.dataset.num;
     //   _squares[num] = "B";
     //   change(_squares);
-    change(num, index);
+    change(num, count);
     //   }
   };
 
@@ -61,9 +65,67 @@ const RenderSquare = (props) => {
         </button>
       );
     });
-    return <div>{buttonCols}</div>;
+    return <div className="btn_list">{buttonCols}</div>;
   });
   return buttons;
 };
 
-export default RenderSquare;
+/**
+ * 0 1 2
+ * 3 4 5
+ * 6 7 8
+ * 
+ */
+const winList = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4 ,8],
+  [2, 4, 6]
+]
+const calcWinnerFor = (squares) =>{
+
+  // if(squares[0] && squares[0] === squares[1] && squares[0] === squares[2]){
+  //   return squares[0];
+  // }
+
+  for(let i = 0 ; i < winList.length ; i++){
+    // const col_0 = winList[i][0];
+    // const col_1 = winList[i][1];
+    // const col_2 = winList[i][2];
+    // 배열의 분해, 배열의 비구조화 코드
+    const [col_0, col_1, col_2] = winList[i]
+    if(squares[col_0] && squares[col_0] === squares[col_1] && squares[col_0] === squares[col_2]){
+      return squares[col_0];
+    }
+  }  
+  return null; 
+}
+
+const calcWinner = (squares)=>{
+  
+  // const 결과 = 원본.map()
+  // 결과의 배열 개수와 원본의 배열 개수는 항상 같다
+  // 내용은 map의 return 결과에 따라 달라진다
+
+  // const 결과 = 원본.filter()
+  // 결과의 배열개수 <= 원본보다 작거나 같다
+  // filter()의 return이 true 일때만 결과에 배열을 추가한다
+
+  // 이 코드에서 비교결과가 true 이면 return true 한 것과 같고
+  // 그때 win 의 값이 result에 담기게 된다
+  // result는 개수가 없거나 1개인 배열이 된다
+  const result =  winList.filter(win=>{ 
+    const [col_0, col_1, col_2] = win
+    return squares[col_0] && 
+           squares[col_0] === squares[col_1] &&
+           squares[col_0] === squares[col_2]
+  })
+
+  return result.length && squares[result[0][0]] 
+}
+
+export {RenderSquare , calcWinner};
